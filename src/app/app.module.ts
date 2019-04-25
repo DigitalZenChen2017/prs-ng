fd92f799f640547201c5c496901a08d4fad207cf
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-04-25 14:53:56 -0400

    Added PRS Logo to NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from './service/user.service';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { UserListComponent } from './feature/user/user-list/user-list.component';
 import { MenuComponent } from './core/menu/menu.component';
 import { UserCreateComponent } from './feature/user/user-create/user-create.component';
-import { VendorListComponent } from './feature/vendor/vendor-list/vendor-list.component';
-import { VendorCreateComponent } from './feature/vendor/vendor-create/vendor-create.component';
-import { ProductListComponent } from './feature/product/product-list/product-list.component';
-import { ProductCreateComponent } from './feature/product/product-create/product-create.component';
-import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
-import { UserDeleteComponent } from './feature/user/user-delete/user-delete.component';
 import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
-import { VendorEditComponent } from './feature/vendor/vendor-edit/vendor-edit.component';
+import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
+import { UserListComponent } from './feature/user/user-list/user-list.component';
+import { VendorCreateComponent } from './feature/vendor/vendor-create/vendor-create.component';
 import { VendorDetailComponent } from './feature/vendor/vendor-detail/vendor-detail.component';
-import { VendorDeleteComponent } from './feature/vendor/vendor-delete/vendor-delete.component';
+import { VendorEditComponent } from './feature/vendor/vendor-edit/vendor-edit.component';
+import { VendorListComponent } from './feature/vendor/vendor-list/vendor-list.component';
+import { ProductCreateComponent } from './feature/product/product-create/product-create.component';
 import { ProductDetailComponent } from './feature/product/product-detail/product-detail.component';
 import { ProductEditComponent } from './feature/product/product-edit/product-edit.component';
-import { PrListComponent } from './feature/pr/pr-list/pr-list.component';
+import { ProductListComponent } from './feature/product/product-list/product-list.component';
 import { PrCreateComponent } from './feature/pr/pr-create/pr-create.component';
-import { PrEditComponent } from './feature/pr/pr-edit/pr-edit.component';
 import { PrDetailComponent } from './feature/pr/pr-detail/pr-detail.component';
-import { PrliListComponent } from './feature/prli/prli-list/prli-list.component';
+import { PrEditComponent } from './feature/pr/pr-edit/pr-edit.component';
+import { PrListComponent } from './feature/pr/pr-list/pr-list.component';
 import { PrliCreateComponent } from './feature/prli/prli-create/prli-create.component';
-import { PrliEditComponent } from './feature/prli/prli-edit/prli-edit.component';
 import { PrliDetailComponent } from './feature/prli/prli-detail/prli-detail.component';
+import { PrliEditComponent } from './feature/prli/prli-edit/prli-edit.component';
+import { PrliListComponent } from './feature/prli/prli-list/prli-list.component';
 
 @NgModule({
   declarations: [
@@ -41,11 +39,9 @@ import { PrliDetailComponent } from './feature/prli/prli-detail/prli-detail.comp
     ProductListComponent,
     ProductCreateComponent,
     UserEditComponent,
-    UserDeleteComponent,
     UserDetailComponent,
     VendorEditComponent,
     VendorDetailComponent,
-    VendorDeleteComponent,
     ProductDetailComponent,
     ProductEditComponent,
     PrListComponent,
--- src/app/feature/product/product-create/product-create.component.ts
+++ src/app/feature/product/product-create/product-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from '../../../model/product.class';
+import { ProductService } from '../../../service/product.service';
+import { Vendor } from 'src/app/model/vendor.class';
 
 @Component({
   selector: 'app-product-create',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+  // tslint:disable-next-line: no-inferrable-types
+  title: string = 'Product Create';
+  resp: any;
+  // product: Product = new Product(0, Vendor, '', '', 0, '', '');
+  // // (0, Vendor, '', '', 0, '', '');
+  product: Product;
 
-  constructor() { }
+  constructor(private productSvc: ProductService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  // Product Create method
+  create() {
+    this.productSvc.create(this.product)
+      .subscribe(resp => {
+        this.resp = resp;
+        this.router.navigate(['/product/list']);
+      });
   }
 
 }
